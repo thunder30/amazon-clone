3a2ebf2dd2b4adbef39a3ef0d15b7bc27784d95d
thunder30 <ngoctinh3004@gmail.com>
2022-01-17 16:55:34 +0700

    Build the Product Components

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import { StarIcon } from '@heroicons/react/solid'
+import NumberFormat from 'react-number-format'
+import primeTag from '../../public/Prime-tag-.png'
 
 function Product({
     product: { id, title, description, category, image, price, rating },
 }) {
+    const [_rating] = useState(Number(rating.rate.toFixed(0)))
+    const [hasPrime] = useState(Math.random() < 0.5)
     return (
-        <div>
-            <p>{category}</p>
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+            <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+                {category}
+            </p>
 
             <Image
                 src={image}
@@ -15,16 +22,46 @@ function Product({
                 width={200}
                 objectFit="contain"
             />
-            <h4>{title}</h4>
+
+            <h4 className="my-3">{title}</h4>
 
             <div className="flex items-center">
-                {Array(Number(rating.rate.toFixed(0)))
+                {Array(_rating)
                     .fill()
                     .map((_, i) => (
                         <StarIcon key={i} className="h-5 text-yellow-400" />
                     ))}
                 {/* <span>{`${rating.rate} - ${rating.count} ratings`}</span> */}
             </div>
+
+            <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+            <div className="mb-5">
+                <NumberFormat
+                    thousandSeparator
+                    thousandsGroupStyle="thousand"
+                    prefix="$"
+                    value={price}
+                    type="text"
+                />
+            </div>
+
+            {hasPrime && (
+                <div className="flex items-center space-x-2 -mt-5">
+                    <Image
+                        src={primeTag}
+                        alt="prime"
+                        width={48}
+                        height={48}
+                        objectFit="contain"
+                    />
+                    <p className="text-xs text-gray-500">
+                        FREE Next-day Delivery
+                    </p>
+                </div>
+            )}
+
+            <button className="mt-auto button">Add to Basket</button>
         </div>
     )
 }
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,9 +1,26 @@
+import Image from 'next/image'
 import Product from './Product'
 
 function ProductFeed({ products }) {
     return (
-        <div>
-            {products.map((product) => (
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+            {products.slice(0, 4).map((product) => (
+                <Product key={product.id} product={product} />
+            ))}
+
+            <img
+                className="md:col-span-full"
+                src="/banner-discover.jpg"
+                alt="image product"
+            />
+
+            <div className="md:col-span-2">
+                {products.slice(4, 5).map((product) => (
+                    <Product key={product.id} product={product} />
+                ))}
+            </div>
+
+            {products.slice(5, products.length).map((product) => (
                 <Product key={product.id} product={product} />
             ))}
         </div>
